4632b45f866371c789d92581b09d2667de15a196
Håkon Harnes <account@harnes.me>
2022-03-03 10:57:39 +0100

    Adds new donation graph

---
+++ components/profile/donations/donationsDistributionTable.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import style from "../../../styles/Donations.module.css";
+import { thousandize } from "../../../util/formatting";
+
+
+const DonationsDistributionTable: React.FC<{ distribution: {org: string, sum: number }[] }> = ({ distribution }) => {
+  distribution = distribution
+    .sort((a,b) => b.sum - a.sum)
+    .map(row => ({ ...row, sum: Math.floor(row.sum) }))
+  
+  return (
+    <div>
+      <table className={style.distribution}>
+        {distribution.map((row) => (
+          <tr key={row.org}>
+            <td>{row.org}</td>
+            <td>{thousandize(row.sum)} kr</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  )
+}
+
+export default DonationsDistributionTable
