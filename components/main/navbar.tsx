41173cbd70f91cf5b35d8e12891eb6331159e9ce
Anniken Emilie Ålberg Hoff <aehoff@stud.ntnu.no>
2022-03-03 12:55:32 +0100

    Fixed navbar mobile view

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -1,26 +1,36 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/Navbar.module.css"
 import logo from "../../public/logo.svg"
 import Link from "next/link";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Menu, X } from "react-feather";
 
 export const Navbar: React.FC = () => {
+  const [expandMenu, setExpandMenu] = useState<boolean>(false);
   return (
-    <nav className={styles.navbar}>
+    <nav className={(expandMenu ? styles.navbar +" "+ styles.navbarExpanded: styles.navbar)}>
+       <div className={styles.logoWrapper}>
       <Image src={logo} 
         className={styles.logo} 
         layout="intrinsic" 
         width={160} 
         height={80}
         alt="Konduit. logo" 
-        priority/>      
-      <ul className={styles.links}>
-        <li><Link href="/organizations" passHref>Anbefalte organisasjoner</Link></li>
-        <li><Link href="/method" passHref>Metode</Link></li>
-        <li><Link href="/about" passHref>Om oss</Link></li>
-        <li><Link href="/FAQ" passHref>FAQ</Link></li>
-        <li><Link href="/profile" passHref>Min side</Link></li>
+        priority/>
+        <div className={styles.expandBtn} onClick={() => setExpandMenu(!expandMenu)}>
+          {expandMenu ?
+            <X width={36} color={"white"} /> :
+            <Menu width={36} color={"white"} />
+          }
+          </div>   
+        </div>   
+      <ul>
+        <li onClick={() => setExpandMenu(false)}><Link href="/organizations" passHref>Anbefalte organisasjoner</Link></li>
+        <li onClick={() => setExpandMenu(false)}><Link href="/method" passHref>Metode</Link></li>
+        <li onClick={() => setExpandMenu(false)}><Link href="/about" passHref>Om oss</Link></li>
+        <li onClick={() => setExpandMenu(false)}><Link href="/FAQ" passHref>FAQ</Link></li>
+        <li onClick={() => setExpandMenu(false)}><Link href="/profile" passHref>Min side</Link></li>
       </ul>
     </nav>
   )
--- components/profile/navbar.tsx
+++ components/profile/navbar.tsx
@@ -4,51 +4,44 @@ import styles from "../../styles/Navbar.module.css";
 import logo from "../../public/logo.svg";
 import Link from "next/link";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Menu, X } from "react-feather";
 
 export const Navbar: React.FC = () => {
-  const { logout, user } = useAuth0();
-  const [linkStyle, setLinkStyle] = useState(styles.links);
+  const { logout } = useAuth0();
+  const [expandMenu, setExpandMenu] = useState<boolean>(false);
 
   return (
     <>
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-        />
-      </head>
-      <nav className={styles.navbar}>
-        <a
-          className={styles.icon}
-          onClick={() =>
-            linkStyle == styles.links
-              ? (setLinkStyle(styles.showlinks), console.log(linkStyle))
-              : (setLinkStyle(styles.links), console.log(linkStyle))
+      <nav className={(expandMenu ? styles.navbar +" "+ styles.navbarExpanded: styles.navbar)} >
+        <div className={styles.logoWrapper}>
+          <Image
+            src={logo}
+            className={styles.logo}
+            layout="intrinsic"
+            width={160}
+            height={80}
+            alt="Konduit. logo"
+            priority
+          />
+          <div className={styles.expandBtn} onClick={() => setExpandMenu(!expandMenu)}>
+          {expandMenu ?
+            <X width={36} color={"white"} /> :
+            <Menu width={36} color={"white"} />
           }
-        >
-          <i className="fa fa-bars"></i>
-        </a>
-        <Image
-          src={logo}
-          className={styles.logo}
-          layout="intrinsic"
-          width={160}
-          height={80}
-          alt="Konduit. logo"
-          priority
-        />
-        <ul className={linkStyle}>
-          <li>
-            <Link href="/profile" passHref>
+          </div>
+        </div>
+        <ul>
+          <li onClick={() => setExpandMenu(false)}>
+            <Link href="/profile"  passHref>
               Mine donasjoner
             </Link>
           </li>
-          <li>
+          <li onClick={() => setExpandMenu(false)}>
             <Link href="/profile/details" passHref>
               Profil
             </Link>
           </li>
-          <li>
+          <li onClick={() => setExpandMenu(false)}>
             <button className={styles.btnlogout} onClick={() => logout()}>
               Logg ut
             </button>
@@ -58,7 +51,3 @@ export const Navbar: React.FC = () => {
     </>
   );
 };
-function usestate(links: string): [any, any] {
-  throw new Error("Function not implemented.");
-}
-
