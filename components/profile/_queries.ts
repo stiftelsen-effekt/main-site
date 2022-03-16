ac4e71531309723face45f92fdbf88fe070971ca
Anniken Emilie Ålberg Hoff <aehoff@stud.ntnu.no>
2022-03-16 13:18:22 +0100

    Fixed errortoast when name is null

--- components/profile/_queries.ts
+++ components/profile/_queries.ts
@@ -1,28 +1,32 @@
 import { User } from "@auth0/auth0-react";
 import { Donor } from "../../models";
 
-export const save = async (data: Donor, user: User , token: string) => {
-    const api = process.env.NEXT_PUBLIC_EFFEKT_API || 'http://localhost:5050'
+export const save = async (data: Donor, user: User, token: string) => {
+  const api = process.env.NEXT_PUBLIC_EFFEKT_API || "http://localhost:5050";
 
-    try {
-      const response = await fetch(
-        `${api}/donors/${user["https://konduit.no/user-id"]}/`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          credentials: "same-origin",
-          body: JSON.stringify(data),
-        }
-      )
-  
-      const donor: Donor = await response.json()
+  try {
+    const response = await fetch(
+      `${api}/donors/${user["https://konduit.no/user-id"]}/`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        credentials: "same-origin",
+        body: JSON.stringify(data),
+      }
+    );
+
+    const donor: Donor = await response.json();
+    if (response.status !== 200) {
+      return null;
+    } else {
       return donor;
-    } catch (e) {
-      return null; 
     }
-}
+  } catch (e) {
+    return null;
+  }
+};
 
-export const test = () => true
+export const test = () => true;
