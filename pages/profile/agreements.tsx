27222bca74bb85c33c3978cc2dab492abca8008c
KC <kcesmena@stud.ntnu.no>
2022-03-31 11:20:30 +0200

    Separates active and inactive agreements (#102)

--- pages/profile/agreements.tsx
+++ pages/profile/agreements.tsx
@@ -3,10 +3,20 @@ import { Layout } from "../../components/profile/layout";
 import { LayoutPage } from "../../types";
 import "react-toastify/dist/ReactToastify.css";
 import { AgreementList } from "../../components/lists/agreementList/agreementList";
-import { AvtaleGiroAgreement, Distribution, Organization, VippsAgreement } from "../../models";
+import {
+  AvtaleGiroAgreement,
+  Distribution,
+  Organization,
+  VippsAgreement,
+} from "../../models";
 import { Spinner } from "../../components/elements/spinner";
 import { useAuth0, User } from "@auth0/auth0-react";
-import { useAgreementsDistributions, useAvtalegiroAgreements, useOrganizations, useVippsAgreements } from "../../_queries";
+import {
+  useAgreementsDistributions,
+  useAvtalegiroAgreements,
+  useOrganizations,
+  useVippsAgreements,
+} from "../../_queries";
 import { useContext } from "react";
 import { ActivityContext } from "../../components/profile/activityProvider";
 import { Children, Lightbox } from "../../components/elements/lightbox";
@@ -20,7 +30,7 @@ const Agreements: LayoutPage = () => {
     data: avtaleGiro,
     isValidating: avtaleGiroRefreshing,
     error: avtaleGiroError,
-  } = useAvtalegiroAgreements(user as User, getAccessTokenSilently)
+  } = useAvtalegiroAgreements(user as User, getAccessTokenSilently);
 
   const {
     loading: vippsLoading,
@@ -38,26 +48,47 @@ const Agreements: LayoutPage = () => {
 
   const kids = new Set<string>();
   if (vipps && avtaleGiro)
-    [...vipps?.map((a: VippsAgreement) => a.KID), ...avtaleGiro?.map((a: AvtaleGiroAgreement) => a.KID)].map((kid) => kids.add(kid));
+    [
+      ...vipps?.map((a: VippsAgreement) => a.KID),
+      ...avtaleGiro?.map((a: AvtaleGiroAgreement) => a.KID),
+    ].map((kid) => kids.add(kid));
 
   const {
     loading: distributionsLoading,
     data: distributions,
     isValidating: distributionsRefreshing,
     error: distributionsError,
-  } = useAgreementsDistributions(user as User, getAccessTokenSilently, !vippsLoading && !avtaleGiroLoading, Array.from(kids))
+  } = useAgreementsDistributions(
+    user as User,
+    getAccessTokenSilently,
+    !vippsLoading && !avtaleGiroLoading,
+    Array.from(kids)
+  );
 
-  const loading = vippsLoading || avtaleGiroLoading || distributionsLoading || organizationsLoading
-  const refreshing = avtaleGiroRefreshing || vippsRefreshing || organizationsRefreshing || distributionsRefreshing
+  const loading =
+    vippsLoading ||
+    avtaleGiroLoading ||
+    distributionsLoading ||
+    organizationsLoading;
+
+  const refreshing =
+    avtaleGiroRefreshing ||
+    vippsRefreshing ||
+    organizationsRefreshing ||
+    distributionsRefreshing;
+
   if (loading || !organizations || !distributions || !vipps || !avtaleGiro)
-    return <><h1>Faste avtaler</h1><Spinner /></>;
+    return (
+      <>
+        <h1>Faste avtaler</h1>
+        <Spinner />
+      </>
+    );
 
-  if (refreshing)
-    setActivity(true)
-  else
-    setActivity(false)
+  if (refreshing) setActivity(true);
+  else setActivity(false);
 
-  const distributionsMap = getDistributionMap(distributions, organizations)
+  const distributionsMap = getDistributionMap(distributions, organizations);
 
   const lightboxChildren: Children = {
     button: "Avslutt",
@@ -82,15 +113,23 @@ const Agreements: LayoutPage = () => {
         <Lightbox>{lightboxChildren}</Lightbox>
         <AgreementList
           title={"Aktive"}
-          vipps={vipps}
-          avtalegiro={avtaleGiro}
+          vipps={vipps.filter(
+            (agreement: VippsAgreement) => agreement.status === "ACTIVE"
+          )}
+          avtalegiro={avtaleGiro.filter(
+            (agreement: AvtaleGiroAgreement) => agreement.active
+          )}
           distributions={distributionsMap}
           supplemental={"Dette er dine aktive betalingsavtaler du har med oss"}
         />
         <AgreementList
           title={"Inaktive"}
-          vipps={vipps}
-          avtalegiro={avtaleGiro}
+          vipps={vipps.filter(
+            (agreement: VippsAgreement) => agreement.status !== "ACTIVE"
+          )}
+          avtalegiro={avtaleGiro.filter(
+            (agreement: AvtaleGiroAgreement) => !agreement.active
+          )}
           distributions={distributionsMap}
           supplemental={
             "Dette er tidligere faste betalingsavtaler du har hatt med oss, som vi ikke lenger trekker deg for"
@@ -104,26 +143,32 @@ const Agreements: LayoutPage = () => {
 Agreements.layout = Layout;
 export default Agreements;
 
-const getDistributionMap = (distributions: Distribution[], organizations: Organization[]) => {
+const getDistributionMap = (
+  distributions: Distribution[],
+  organizations: Organization[]
+) => {
   const map = new Map<string, Distribution>();
-  
-  for (let i = 0; i < distributions.length; i++) {
-    let dist = distributions[i]
 
-    let newDist = { kid: "", organizations: organizations.map(org => ({
-      id: org.id,
-      name: org.name,
-      share: "0"
-    }))}
+  for (let i = 0; i < distributions.length; i++) {
+    let dist = distributions[i];
+
+    let newDist = {
+      kid: "",
+      organizations: organizations.map((org) => ({
+        id: org.id,
+        name: org.name,
+        share: "0",
+      })),
+    };
 
     for (let j = 0; j < dist.organizations.length; j++) {
-      let org = dist.organizations[j]
-      let index = newDist.organizations.map(o => o.id).indexOf(org.id)
-      newDist.organizations[index].share = org.share
+      let org = dist.organizations[j];
+      let index = newDist.organizations.map((o) => o.id).indexOf(org.id);
+      newDist.organizations[index].share = org.share;
     }
 
-    map.set(dist.kid, {...newDist})
+    map.set(dist.kid, { ...newDist });
   }
 
-  return map
-}
+  return map;
+};
