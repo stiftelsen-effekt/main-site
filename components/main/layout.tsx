5f669b1d736376de2420e566fe3085ca2257ffd9
Alwin Nedrum <nedrum@gmail.com>
2022-04-11 16:57:46 +0200

    added  GIButton to layout.tsx

--- components/main/layout.tsx
+++ components/main/layout.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { Footer } from "../footer";
-import { Navbar } from "./navbar";
+import React from 'react'
+import { Footer } from '../footer'
+import { Navbar } from './navbar'
 import styles from '../../styles/Layout.module.css'
-import { LayoutElement } from "../../types";
-
+import { LayoutElement } from '../../types'
+import { GiButton } from '../give-now-button/gi-button'
 export const Layout: LayoutElement = ({ children }) => {
   return (
     <div className={styles.container}>
+      <GiButton url="/donations" />
       <Navbar />
       <main className={styles.main}>{children}</main>
       <Footer />
