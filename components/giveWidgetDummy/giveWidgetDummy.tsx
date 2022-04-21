4b81177db28cc0e69e1d7dda14ee6cf2a4e2da5f
Alwin Nedrum <nedrum@gmail.com>
2022-04-21 12:02:59 +0200

    Added buttons to layout

--- components/giveWidgetDummy/giveWidgetDummy.tsx
+++ components/giveWidgetDummy/giveWidgetDummy.tsx
@@ -3,14 +3,28 @@ import styles from '../../styles/GiveWidgetDummy.module.css'
 
 export const GiveWidgetDummy: React.FC = () => {
   return (
-    <aside>
+    <aside className={styles.donations__widget}>
       <form>
-        <input type="radio" name="fast" id="fast" />
-        <label htmlFor="fast">Gi en fast månedlig sum</label>
-        <br />
-        <input type="radio" name="engangs" id="engangs" />
-        <label htmlFor="fast">Gi et engangsbeløp</label>
-        <button></button>
+        <div className={styles.donationtype}>
+          <label htmlFor="fast">
+            <input type="radio" name="fastdonasjon" id="fast" />
+            Gi en fast månedlig sum
+          </label>
+          <hr />
+
+          <label htmlFor="engangs">
+            <input type="radio" name="engangsdonasjon" id="engangs" />
+            Gi et engangsbeløp
+          </label>
+        </div>
+
+        <div className={styles.paymethod}>
+          <button className={styles.paymethod__btn + ' ' + styles.bank}>
+            BANK
+          </button>
+          <button className={styles.paymethod__btn + ' ' + styles.vipps} />
+          <button className={styles.paymethod__btn + ' ' + styles.paypal} />
+        </div>
       </form>
     </aside>
   )
