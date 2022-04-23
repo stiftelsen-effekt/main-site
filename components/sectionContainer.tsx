c4af6470f8d9550e6289bf905bf6cf02a91d2023
Alwin Nedrum <nedrum@gmail.com>
2022-04-23 14:56:03 +0200

    added nodivider to module

--- components/sectionContainer.tsx
+++ components/sectionContainer.tsx
@@ -4,19 +4,24 @@ import styles from '../styles/SectionContainer.module.css'
 interface Section {
   heading: string
   inverted?: boolean
+  nodivider?: boolean
 }
 
 export const SectionContainer: React.FC<Section> = ({
   heading,
   inverted,
+  nodivider,
   children,
 }) => {
   let background
+  let dividerLine = styles.divider
+
   inverted ? (background = styles.inverted) : null
+  nodivider ? (dividerLine = '') : null
 
   return (
     <section className={styles.section__container + ' ' + background}>
-      <h2 className={styles.section__heading}>{heading}</h2>
+      <h2 className={styles.section__heading + ' ' + dividerLine}>{heading}</h2>
       <div className={styles.section__content}>{children}</div>
     </section>
   )
