2acb9905de03fcef03d84c7ae36d7ef3613037a3
Håkon Harnes <account@harnes.me>
2022-05-31 14:41:09 +0200

    Adds contact info component and updates schema

--- studio/schemas/types/contactinfo.ts
+++ studio/schemas/types/contactinfo.ts
@@ -4,6 +4,16 @@ export default {
   title: 'Contact information',
   fields: [
     {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    },
+    {
+      name: 'description',
+      type: 'string',
+      title: 'Description'
+    },
+    {
       name: 'phone',
       type: 'string',
       title: 'Phone'
