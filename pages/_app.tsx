7efa2c7096d787a923c1256dab13c4ac8fbb6d25
Morten Kjelling <mortenok@gmail.com>
2022-06-08 08:58:38 +0200

    remove console.log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import { Layout } from "../components/main/layout";
 function MyApp({ Component, pageProps }: AppProps) {
   // Gets the page layout from the component, defaults to the main layout
   const PageLayout = (Component as LayoutPage).layout || Layout;
-  console.log(pageProps.data);
   return (
     <PageLayout footerData={pageProps.data.footer[0]}>
       <Component {...pageProps} />
