e9c9b0c8e4a50213e5982aad16bb873a136c0e16
Alwin Nedrum <nedrum@gmail.com>
2022-06-09 12:50:37 +0200

    Added a test site for buttons

---
+++ pages/button_test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ButtonTests = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <a href="#">Link Button</a>
+    </>
+  );
+};
+
+export default ButtonTests;
