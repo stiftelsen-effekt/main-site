b0bf6a15af995acd7cb5505eeecc083fb65e928e
Håkon Harnes <account@harnes.me>
2022-06-17 13:29:29 +0200

    Breaks the cycle

--- components/profile/loginError.tsx
+++ components/profile/loginError.tsx
@@ -1,17 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import elements from "../../styles/Elements.module.css";
-import { EffektButton } from "../elements/effektbutton";
+import { EffektButton, EffektButtonType } from "../elements/effektbutton";
 
 export const LoginError: React.FC<{ message: string }> = ({ message }) => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, logout } = useAuth0();
 
   return (
     <div className={elements["center-wrapper"]}>
       <p>{message}</p>
-      <EffektButton onClick={loginWithRedirect} cy="btn-login">
-        Logg in
-      </EffektButton>
+      <div>
+        <EffektButton onClick={logout} type={EffektButtonType.SECONDARY}>
+          Logg ut
+        </EffektButton>
+        <EffektButton onClick={loginWithRedirect} cy="btn-login">
+          Logg inn
+        </EffektButton>
+      </div>
     </div>
   );
 };
