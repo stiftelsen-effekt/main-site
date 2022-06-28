fe7c11dcf8eb5bdf84cb7d4d8c95cb2eb64776ba
Håkon Harnes <account@harnes.me>
2022-06-28 14:20:03 +0200

    Fixes wrong import in footer props

--- types.ts
+++ types.ts
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import { ReactElement } from "react";
-import { FooterProps } from "./components/shared/footer";
+import { FooterProps } from "./components/shared/layout/Footer/Footer";
 
 export type LayoutPage<P = {}> = NextPage<P> & { layout: LayoutElement };
 export type LayoutElement = React.FC<{ children: ReactElement, footerData: FooterProps }>;
