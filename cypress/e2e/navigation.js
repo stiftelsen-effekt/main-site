9d7f62ffe9f3678e02e4fedd938c52988170b7a5
Philip <44257880+philiphand@users.noreply.github.com>
2022-07-24 23:11:45 +0200

    Adds more Cypress tests

--- cypress/e2e/navigation.js
+++ cypress/e2e/navigation.js
@@ -3,7 +3,96 @@ describe("Navigation", () => {
         cy.visit(`/`)
     });
 
-    it("Click articles button", () => {
+    it("Tests if CookieBanner works correctly", () => {
+        // CookieBanner should be visible
+        cy.get('[data-cy=cookiebanner-container]')
+            .invoke('css', 'display')
+            .should('equal', 'flex')
+
+        cy.get("[data-cy=accept-cookies]").click()
+
+        // CookieBanner should be hidden
+        cy.get('[data-cy=cookiebanner-container]')
+            .invoke('css', 'display')
+            .should('equal', 'none')
+    });
+
+    it("Tests if NavBar and scrolling works correctly", () => {
+        // NavBar should be visible
+        cy.get('[data-cy=header]')
+            .invoke('css', 'transform')
+            .should('equal', 'none')
+
+        cy.scrollTo(0, 500)
+        cy.wait(100)
+
+        // Navbar should be hidden
+        cy.get('[data-cy=header]')
+            .invoke('css', 'transform')
+            .should('equal', 'matrix(1, 0, 0, 1, 0, -89.3333)')
+
+        cy.scrollTo(0, 0)
+        cy.wait(100)
+
+        // NavBar should be visible
+        cy.get('[data-cy=header]')
+            .invoke('css', 'transform')
+            .should('equal', 'none')
+        
+        cy.scrollTo(0, 500)
+        cy.wait(100)
+
+        // Navbar should be hidden
+        cy.get('[data-cy=header]')
+            .invoke('css', 'transform')
+            .should('equal', 'matrix(1, 0, 0, 1, 0, -89.3333)')
+
+        cy.get('[data-cy=navigate-to-top]').click()
+        cy.wait(100)
+        cy.window().its('scrollY').should('equal', 0)
+
+        // NavBar should be visible
+        cy.get('[data-cy=header]')
+            .invoke('css', 'transform')
+            .should('equal', 'none')
+    });
+
+    it("Tests buttons for opening and closing Widget", () => {
+        // Widget should be closed
+        cy.get('[data-cy=widget-pane]')
+            .invoke('css', 'transform')
+            .should('equal', 'matrix(1, 0, 0, 1, 762, 0)')
+
+        cy.get("[data-cy=send-donation-button]").click()
+
+        // Widget should be open
+        cy.get('[data-cy=widget-pane]')
+            .invoke('css', 'transform')
+            .should('equal', 'matrix(1, 0, 0, 1, 0, 0)')
+
+        cy.get("[data-cy=close-widget]").click()
+
+        // Widget should be closed
+        cy.get('[data-cy=widget-pane]')
+            .invoke('css', 'transform')
+            .should('equal', 'matrix(1, 0, 0, 1, 762, 0)')
+    
+        cy.get("[data-cy=gi-button]").click()
+
+        // Widget should be open
+        cy.get('[data-cy=widget-pane]')
+            .invoke('css', 'transform')
+            .should('equal', 'matrix(1, 0, 0, 1, 0, 0)')
+
+        cy.get("[data-cy=close-widget]").click()
+
+        // Widget should be closed
+        cy.get('[data-cy=widget-pane]')
+            .invoke('css', 'transform')
+            .should('equal', 'matrix(1, 0, 0, 1, 762, 0)')
+    })
+
+    it("Tests navigation between pages", () => {
         cy.get("[data-cy=articles-link]").within(() => {
             cy.get("a").click({ force: true })
         })
@@ -43,74 +132,11 @@ describe("Navigation", () => {
             cy.get("a").click({force: true})
         })
         cy.url().should('include', '/support')
-
-        // CookieBanner should be visible
-        cy.get('[data-cy=cookiebanner-container]')
-            .invoke('css', 'display')
-            .should('equal', 'flex')
-
-        cy.get("[data-cy=accept-cookies]").click()
-
-        // CookieBanner should be hidden
-        cy.get('[data-cy=cookiebanner-container]')
-            .invoke('css', 'display')
-            .should('equal', 'none')
-        
-        // NavBar should be visible
-        cy.get('[data-cy=header]')
-            .invoke('css', 'transform')
-            .should('equal', 'none')
-
-        cy.scrollTo(0, 500)
-        cy.wait(100)
-
-        // Navbar should be hidden
-        cy.get('[data-cy=header]')
-            .invoke('css', 'transform')
-            .should('equal', 'matrix(1, 0, 0, 1, 0, -89.3333)')
-
-        cy.scrollTo(0, 0)
-        cy.wait(100)
-
-        // NavBar should be visible
-        cy.get('[data-cy=header]')
-        .invoke('css', 'transform')
-        .should('equal', 'none')
     });
 
-
-    it("Tests buttons for opening and closing Widget", () => {
-        // Widget should be closed
-        cy.get('[data-cy=widget-pane]')
-            .invoke('css', 'transform')
-            .should('equal', 'matrix(1, 0, 0, 1, 762, 0)')
-
-        cy.get("[data-cy=send-donation-button]").click()
-
-        // Widget should be open
-        cy.get('[data-cy=widget-pane]')
-            .invoke('css', 'transform')
-            .should('equal', 'matrix(1, 0, 0, 1, 0, 0)')
-
-        cy.get("[data-cy=close-widget]").click()
-
-        // Widget should be closed
-        cy.get('[data-cy=widget-pane]')
-            .invoke('css', 'transform')
-            .should('equal', 'matrix(1, 0, 0, 1, 762, 0)')
-    
-        cy.get("[data-cy=gi-button]").click()
-
-        // Widget should be open
-        cy.get('[data-cy=widget-pane]')
-            .invoke('css', 'transform')
-            .should('equal', 'matrix(1, 0, 0, 1, 0, 0)')
-
-        cy.get("[data-cy=close-widget]").click()
-
-        // Widget should be closed
-        cy.get('[data-cy=widget-pane]')
-            .invoke('css', 'transform')
-            .should('equal', 'matrix(1, 0, 0, 1, 762, 0)')
-    })
+    it("Tests newsletter signup", () => {
+        cy.get("[data-cy=newsletter-input]").type("test@gieffektivt.no")
+        cy.get('[data-cy=newsletter-input]').invoke('text').should('equal', "test@gieffektivt.no")
+        cy.get("[data-cy=newsletter-submit]").click()
+    });
 });
