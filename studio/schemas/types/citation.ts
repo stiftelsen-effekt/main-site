678db20b5d3ef1b6283a291092fcfb3517746e2b
Håkon Harnes <account@harnes.me>
2022-12-01 23:48:01 +0100

    Updates typings to explicitly set them to any

--- studio/schemas/types/citation.ts
+++ studio/schemas/types/citation.ts
@@ -30,74 +30,74 @@ export default {
       name: 'author',
       type: 'string',
       title: 'Author',
-      hidden: ({ parent }) => typeof parent.type === 'undefined',
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined',
     },
     {
       name: 'title',
       type: 'string',
       title: 'Title',
-      hidden: ({ parent }) => typeof parent.type === 'undefined',
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined',
     },
     {
       name: 'journal',
       type: 'string',
       title: 'Journal',
-      hidden: ({ parent }) => typeof parent.type === 'undefined' || parent.type !== 'article',
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined' || parent.type !== 'article',
     },
     {
       name: 'year',
       type: 'string',
       title: 'Year',
-      hidden: ({ parent }) => typeof parent.type === 'undefined'
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined'
     },
     {
       name: 'volume',
       type: 'string',
       title: 'Volume',
-      hidden: ({ parent }) => typeof parent.type === 'undefined' || parent.type !== 'article'
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined' || parent.type !== 'article'
     },
     {
       name: 'number',
       type: 'string',
       title: 'Number',
-      hidden: ({ parent }) => typeof parent.type === 'undefined' || parent.type !== 'article'
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined' || parent.type !== 'article'
     },
     {
       name: 'pages',
       type: 'string',
       title: 'Pages',
-      hidden: ({ parent }) => typeof parent.type === 'undefined' || (parent.type !== 'article' && parent.type !== 'book')
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined' || (parent.type !== 'article' && parent.type !== 'book')
     },
     {
       name: 'publisher',
       type: 'string',
       title: 'Publisher',
-      hidden: ({ parent }) => typeof parent.type === 'undefined' || parent.type !== 'book'
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined' || parent.type !== 'book'
     },
     {
       name: 'address',
       type: 'string',
       title: 'Address',
-      hidden: ({ parent }) => typeof parent.type === 'undefined' || parent.type !== 'book'
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined' || parent.type !== 'book'
     },
     {
       name: 'accessed',
       type: 'datetime',
       title: 'Accessed',
-      hidden: ({ parent }) => typeof parent.type === 'undefined' || parent.type !== 'website'
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined' || parent.type !== 'website'
     },
     {
       name: 'timestamp',
       type: 'string',
       title: 'Timestamp',
       description: 'Timestamp for the cited information e.g. 32:45',
-      hidden: ({ parent }) => typeof parent.type === 'undefined' || (parent.type !== 'video' && parent.type !== 'podcast')
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined' || (parent.type !== 'video' && parent.type !== 'podcast')
     },
     {
       name: 'url',
       type: 'url',
       title: 'URL',
-      hidden: ({ parent }) => typeof parent.type === 'undefined' || parent.type === 'book'
+      hidden: ({ parent }: any) => typeof parent.type === 'undefined' || parent.type === 'book'
     }
   ],
   options: {
