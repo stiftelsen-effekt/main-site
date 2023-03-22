b514ed9bd072fedf8646a140b540651bbcac120d
Håkon Harnes <account@harnes.me>
2023-03-22 18:43:01 +0100

    Adds donation impact section

---
+++ components/main/blocks/ImpactWidget/ImpactWidgetOutput.tsx
@@ -0,0 +1,163 @@
+import React, { useEffect, useState } from "react";
+import AnimateHeight from "react-animate-height";
+import styles from "./ImpactWidget.module.scss";
+import { Links } from "../Links/Links";
+import { Spinner } from "../../../shared/components/Spinner/Spinner";
+import { thousandize } from "../../../../util/formatting";
+
+export type SanityIntervention = {
+  title: string;
+  organization_name: string;
+  abbreviation: string;
+  template_string: string;
+};
+
+type Intervention = {
+  title: string;
+  organizationName: string;
+  abbreviation: string;
+  pricePerOutput?: number;
+  outputStringTemplate: string;
+};
+
+export const ImpactWidgetOutput: React.FC<{ sum: number; interventions: SanityIntervention[] }> = ({
+  sum,
+  interventions,
+}) => {
+  console.log("interventions", interventions);
+
+  const [contextExpanded, setContextExpanded] = useState(false);
+  const [interventionCosts, setInterventionCosts] = useState<Map<string, number>>(new Map());
+  const [selectedIntervention, setSelectedIntervention] = useState<string>(interventions[0].title);
+
+  const mappedInterventions: Intervention[] = interventions.map((i: SanityIntervention) => ({
+    title: i.title,
+    abbreviation: i.abbreviation,
+    pricePerOutput: interventionCosts.get(i.abbreviation),
+    outputStringTemplate: i.template_string,
+    organizationName: i.organization_name,
+  }));
+
+  useEffect(() => {
+    const url = `https://impact.gieffektivt.no/api/evaluations?${interventions
+      .map((i: any) => `charity_abbreviation=${i.abbreviation}&`)
+      .join("")}currency=NOK`;
+    fetch(url).then((res) => {
+      res.json().then((data) => {
+        const costs = new Map();
+        const evaluations = data.evaluations;
+        mappedInterventions.forEach((i: any) => {
+          // For each intervention, filter the evaluations for a given charity
+          const filtered = evaluations.filter(
+            (e: any) => e.charity.abbreviation === i.abbreviation,
+          );
+          // Then order the list to get the most recent
+          const ordered = filtered.sort((a: any, b: any) => a.start_year - b.start_year);
+          // Get the most recent evaluation
+          const evaluation = ordered[0];
+          // Set the cost to the most recent evaluation converted cost (cost in NOK per output)
+          costs.set(i.abbreviation, evaluation.converted_cost_per_output);
+        });
+        setInterventionCosts(costs);
+      });
+    });
+  }, []);
+
+  const currentIntervention = mappedInterventions.find(
+    (i: any) => i.title === selectedIntervention,
+  ) as Intervention;
+
+  if (!currentIntervention) {
+    setSelectedIntervention(mappedInterventions[0].title);
+    return <Spinner />;
+  }
+
+  const output = currentIntervention.pricePerOutput
+    ? Math.round(sum / currentIntervention.pricePerOutput)
+    : 0;
+  const loading = typeof currentIntervention.pricePerOutput === "undefined";
+  let outputString;
+  if (isNaN(output)) {
+    outputString = "0";
+  } else {
+    outputString = output.toString();
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.select}>
+        {interventions.map((i: any) => (
+          <button
+            data-cy={`${i.title}-button`}
+            key={i.title}
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              e.currentTarget.blur();
+              setSelectedIntervention(i.title);
+            }}
+            className={i.title === selectedIntervention ? styles.selected : ""}
+          >
+            {i.title}
+          </button>
+        ))}
+      </div>
+      <div className={styles.output}>
+        {loading && (
+          <div className={styles.spinnerWrapper}>
+            <Spinner />
+          </div>
+        )}
+        {!loading && (
+          <>
+            <span className="detailheader">{currentIntervention.organizationName}</span>
+            <div className={styles.paragraphWrapper}>
+              <span data-cy="impact-output" className={styles.paragraphNumber}>
+                {thousandize(parseInt(outputString))}
+              </span>
+              <div className={styles.explanatory}>
+                <p>
+                  <span className={styles.innerParagraphNumber}>{outputString}</span>
+                  {currentIntervention.outputStringTemplate}
+                </p>
+                <span
+                  className={contextExpanded ? styles.captionopen : ""}
+                  onClick={() => setContextExpanded(!contextExpanded)}
+                >
+                  Hvordan er dette sammenlignet med andre organisasjoner?&nbsp;&nbsp;
+                </span>
+              </div>
+            </div>
+            <AnimateHeight duration={300} height={contextExpanded ? "auto" : 0} animateOpacity>
+              <div className={styles.context}>
+                Tallene er basert på analysene til GiveWell. De gir et omtrentlig bilde på hva våre
+                anbefalte organisasjoner får ut av pengene. Alle tre tiltak er topp anbefalt som de
+                mest kostnadseffektive måtene å redde liv eller forbedre den økonomiske situasjonen
+                til ekstremt fattige. Mange bistandsorganisasjoner viser til overdrevne og
+                misvisende tall i sin markedsføring. Bak våre tall ligger tusenvis av timer med
+                undersøkelser og inkluderer alle kostnader, inkludert planlegging, innkjøp,
+                distribusjon, opplæring og kontroll.
+              </div>
+              <Links
+                links={[
+                  {
+                    _key: "givewell",
+                    _type: "link",
+                    title: "GiveWell's analyser",
+                    url: "https://www.givewell.org/impact-estimates",
+                    newtab: true,
+                  },
+                  {
+                    _key: "organiasjoner",
+                    _type: "navitem",
+                    title: "Anbefalte organisasjoner",
+                    pagetype: "organizations",
+                    slug: "organizations",
+                  },
+                ]}
+              ></Links>
+            </AnimateHeight>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
--- components/main/blocks/WealthCalculator/WealthCalculator.tsx
+++ components/main/blocks/WealthCalculator/WealthCalculator.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { NumericFormat } from "react-number-format";
 import { useDebouncedCallback } from "use-debounce";
 import { thousandize } from "../../../../util/formatting";
+import { EffektButton } from "../../../shared/components/EffektButton/EffektButton";
 import { EffektSlider } from "../../../shared/components/EffektSlider/EffektSlider";
 import { AreaChart } from "../../../shared/components/Graphs/Area/AreaGraph";
+import { WidgetContext } from "../../layout/layout";
+import { ImpactWidgetOutput } from "../ImpactWidget/ImpactWidgetOutput";
 import { wealthMountainGraphData } from "./data";
 import styles from "./WealthCalculator.module.scss";
 
@@ -11,6 +14,7 @@ export const WealthCalculator: React.FC<{ showImpact: boolean }> = ({ showImpact
   const [incomeInput, setIncomeInput] = useState<number | undefined>();
   const income = incomeInput || 0;
   const [donationPercentage, setDonationPercentage] = useState(10);
+  const [widgetOpen, setWidgetOpen] = useContext(WidgetContext);
   const [chartSize, setChartSize] = useState<{
     width: number | undefined;
     height: number | undefined;
@@ -44,80 +48,109 @@ export const WealthCalculator: React.FC<{ showImpact: boolean }> = ({ showImpact
   }, []);
 
   return (
-    <div className={styles.calculator}>
-      <div className={styles.calculator__input}>
-        <div className={styles.calculator__input__inner}>
-          <h5>Rikdomskalkulator.</h5>
-          <span className={styles.calculator__input__subtitle}>
-            Hvor rik er du sammenlignet med resten av verden? Test.
-          </span>
+    <div className={styles.wrapper}>
+      <div className={styles.calculator}>
+        <div className={styles.calculator__input}>
+          <div className={styles.calculator__input__inner}>
+            <h5>Rikdomskalkulator.</h5>
+            <span className={styles.calculator__input__subtitle}>
+              Hvor rik er du sammenlignet med resten av verden? Test.
+            </span>
 
-          <div className={styles.calculator__input__group}>
-            <div className={styles.calculator__input__group__input__income__wrapper}>
-              <NumericFormat
-                type={"tel"}
-                placeholder={"Inntekt"}
-                value={incomeInput}
-                className={styles.calculator__input__group__input__text}
-                thousandSeparator={" "}
-                onValueChange={(values) => {
-                  setIncomeInput(values.floatValue);
-                }}
-              />
-              <span>kr</span>
-            </div>
-            <i>Oppgi total inntekt før skatt for husholdningen din.</i>
-          </div>
-
-          <div className={styles.calculator__input__group}>
-            <div>
-              <span>Om du ga bort </span>
-              <div className={styles.calculator__input__group__percentage_input_wrapper}>
-                <input
-                  type={"text"}
-                  className={styles.calculator__input__group__percentage_input}
-                  value={donationPercentage.toString()}
-                  onChange={(e) => {
-                    if (e.target.value !== "" || !isNaN(parseInt(e.target.value))) {
-                      setDonationPercentage(parseInt(e.target.value));
-                    } else if (e.target.value === "") {
-                      setDonationPercentage(0);
-                    }
+            <div className={styles.calculator__input__group}>
+              <div className={styles.calculator__input__group__input__income__wrapper}>
+                <NumericFormat
+                  type={"tel"}
+                  placeholder={"Inntekt"}
+                  value={incomeInput}
+                  className={styles.calculator__input__group__input__text}
+                  thousandSeparator={" "}
+                  onValueChange={(values) => {
+                    setIncomeInput(values.floatValue);
                   }}
                 />
-                <span>%</span>
+                <span>kr</span>
               </div>
-              <span>
-                av din inntekt kan du donere{" "}
-                {thousandize(Math.round(income * (donationPercentage / 100)))} kr til effektiv
-                bistand i året og fortsatt være blant de{" "}
-                {calculateWealthPercentile(
-                  wealthMountainGraphData,
-                  income * (1 - donationPercentage / 100),
-                ).toLocaleString("no-NB")}
-                % rikeste i verden.
-              </span>
+              <i>Oppgi total inntekt før skatt for husholdningen din.</i>
             </div>
 
-            <EffektSlider
-              min={0}
-              max={50}
-              value={donationPercentage}
-              onChange={setDonationPercentage}
+            <div className={styles.calculator__input__group}>
+              <div>
+                <span>Om du ga bort </span>
+                <div className={styles.calculator__input__group__percentage_input_wrapper}>
+                  <input
+                    type={"text"}
+                    className={styles.calculator__input__group__percentage_input}
+                    value={donationPercentage.toString()}
+                    onChange={(e) => {
+                      if (e.target.value !== "" || !isNaN(parseInt(e.target.value))) {
+                        setDonationPercentage(parseInt(e.target.value));
+                      } else if (e.target.value === "") {
+                        setDonationPercentage(0);
+                      }
+                    }}
+                  />
+                  <span>%</span>
+                </div>
+                <span>
+                  av din inntekt kan du donere{" "}
+                  {thousandize(Math.round(income * (donationPercentage / 100)))} kr til effektiv
+                  bistand i året og fortsatt være blant de{" "}
+                  {calculateWealthPercentile(
+                    wealthMountainGraphData,
+                    income * (1 - donationPercentage / 100),
+                  ).toLocaleString("no-NB")}
+                  % rikeste i verden.
+                </span>
+              </div>
+
+              <EffektSlider
+                min={0}
+                max={50}
+                value={donationPercentage}
+                onChange={setDonationPercentage}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.calculator__output} ref={outputRef}>
+          <AreaChart
+            data={wealthMountainGraphData}
+            lineInput={income || 0}
+            donationPercentage={donationPercentage / 100}
+            wealthPercentile={calculateWealthPercentile(wealthMountainGraphData, income || 0)}
+            size={chartSize}
+          />
+        </div>
+      </div>
+      {showImpact && (
+        <div className={styles.calculator__impact}>
+          <div className={styles.calculator__impact__description}>
+            <h3>Din impact.</h3>
+            <p>
+              Med {thousandize(Math.round(income * (donationPercentage / 100)))} kroner i året
+              donert til effektiv bistand kan du påvirke mange liv der det trengs mest du kan for
+              eksempel bidra med myggnett, A-vitamin tilskudd eller vaksinering.
+            </p>
+            <EffektButton onClick={() => setWidgetOpen(true)}>Sett opp fast donasjon</EffektButton>
+          </div>
+          <div className={styles.calculator__impact__output}>
+            <ImpactWidgetOutput
+              sum={income * (donationPercentage / 100)}
+              interventions={[
+                {
+                  title: "Mygnett",
+                  abbreviation: "AMF",
+                  organization_name: "Against Malaria Foundation",
+                  template_string:
+                    "myggnett distribueres i malariautsatte områder. Forebygger spredning av malaria og forhindrer sykdom og død.                  ",
+                },
+              ]}
             />
           </div>
         </div>
-      </div>
-
-      <div className={styles.calculator__output} ref={outputRef}>
-        <AreaChart
-          data={wealthMountainGraphData}
-          lineInput={income || 0}
-          donationPercentage={donationPercentage / 100}
-          wealthPercentile={calculateWealthPercentile(wealthMountainGraphData, income || 0)}
-          size={chartSize}
-        />
-      </div>
+      )}
     </div>
   );
 };
