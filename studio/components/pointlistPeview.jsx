37f87511626a30efcfb5a6554ee359b90a6fe5f5
Adam Alfredsson <alfredsson.adam@gmail.com>
2023-04-13 15:49:35 +0200

    Format files

--- studio/components/pointlistPeview.jsx
+++ studio/components/pointlistPeview.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
-import { Grid, Stack, Text, Flex, Box } from '@sanity/ui'
-import { List } from 'react-feather'
+import React from "react";
+import { Grid, Stack, Text, Flex, Box } from "@sanity/ui";
+import { List } from "react-feather";
 // These are react components
 
 export const PointlistPreview = React.forwardRef((props, ref) => {
-    if (!props.value.points)
-      return <Text><List size={24} /></Text>
+  if (!props.value.points)
+    return (
+      <Text>
+        <List size={24} />
+      </Text>
+    );
 
-    const maxcharacters = 64
-    const npoints = props.value.points.length
-    const subtitlelength = Math.round(maxcharacters / npoints)
-    const titlelength = Math.round(maxcharacters / npoints - 2)
+  const maxcharacters = 64;
+  const npoints = props.value.points.length;
+  const subtitlelength = Math.round(maxcharacters / npoints);
+  const titlelength = Math.round(maxcharacters / npoints - 2);
 
-    return <Flex direction={'row'} align={'center'}>
+  return (
+    <Flex direction={"row"} align={"center"}>
       <Box style={{ flexShrink: 0 }}>
         <List size={24} />
       </Box>
       <Grid columns={props.value.points.length} marginLeft={3}>
-        {props.value.points.map(p => <Stack rows={2} space={2} marginRight={2}>
-          <Text size={1} style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>{p.heading ? p.heading.length > titlelength ? p.heading.substr(0, titlelength-2) + "..." : p.heading : '-'}</Text>
-          <Text size={1} muted style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>{p.paragraph ? p.paragraph.length > subtitlelength ? p.paragraph.substr(0, subtitlelength - 2) + "..." : p.paragraph : '-'}</Text>
-        </Stack>)}
+        {props.value.points.map((p) => (
+          <Stack rows={2} space={2} marginRight={2}>
+            <Text size={1} style={{ whiteSpace: "nowrap", textOverflow: "ellipsis" }}>
+              {p.heading
+                ? p.heading.length > titlelength
+                  ? p.heading.substr(0, titlelength - 2) + "..."
+                  : p.heading
+                : "-"}
+            </Text>
+            <Text size={1} muted style={{ whiteSpace: "nowrap", textOverflow: "ellipsis" }}>
+              {p.paragraph
+                ? p.paragraph.length > subtitlelength
+                  ? p.paragraph.substr(0, subtitlelength - 2) + "..."
+                  : p.paragraph
+                : "-"}
+            </Text>
+          </Stack>
+        ))}
       </Grid>
     </Flex>
-  }
-)
+  );
+});
--- studio/components/testimonialsPreview.jsx
+++ studio/components/testimonialsPreview.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import { Text, Flex, Grid, Box, Stack } from '@sanity/ui'
-import { MessageCircle } from 'react-feather'
+import React from "react";
+import { Text, Flex, Grid, Box, Stack } from "@sanity/ui";
+import { MessageCircle } from "react-feather";
 
 export const TestimonialsPreview = React.forwardRef((props, ref) => {
   if (!props.value) {
-    return <Text muted size={1}>No testimonials</Text>
+    return (
+      <Text muted size={1}>
+        No testimonials
+      </Text>
+    );
   }
 
-  const testimonials = props.value.testimonials
+  const testimonials = props.value.testimonials;
 
-
-  return <Flex direction={'row'} align={'center'}>
-    <Box style={{ flexShrink: 0 }}>
-      <MessageCircle size={24} />
-    </Box>
-    <Grid columns={testimonials.length} gap={3} marginLeft={3}>
-      {testimonials.map(testimonial => (
-        <Stack rows={2} space={2}>
-          <Text textOverflow={"ellipsis"} size={1}>{testimonial.quotee || '-'}</Text>
-          <Text textOverflow={"ellipsis"} size={1} muted>{testimonial.quote || '-'}</Text>
-        </Stack>
-      ))}
-    </Grid>
-  </Flex>
-  }
-)
+  return (
+    <Flex direction={"row"} align={"center"}>
+      <Box style={{ flexShrink: 0 }}>
+        <MessageCircle size={24} />
+      </Box>
+      <Grid columns={testimonials.length} gap={3} marginLeft={3}>
+        {testimonials.map((testimonial) => (
+          <Stack rows={2} space={2}>
+            <Text textOverflow={"ellipsis"} size={1}>
+              {testimonial.quotee || "-"}
+            </Text>
+            <Text textOverflow={"ellipsis"} size={1} muted>
+              {testimonial.quote || "-"}
+            </Text>
+          </Stack>
+        ))}
+      </Grid>
+    </Flex>
+  );
+});
