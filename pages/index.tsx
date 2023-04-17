cb7659807fbdce00ca959e11b5aa23e6791c5030
Håkon Harnes <account@harnes.me>
2023-04-17 17:12:18 +0200

    Fixes links issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ import { MainHeader } from "../components/shared/layout/Header/Header";
 import { Layout } from "../components/main/layout/layout";
 import { ImpactWidgetProps } from "../components/main/blocks/ImpactWidget/ImpactWidget";
 import { filterPageToSingleItem, filterWidgetToSingleItem } from "./_app";
-import { linksSelectorQuery, widgetQuery } from "../_queries";
+import { linksContentQuery, linksSelectorQuery, widgetQuery } from "../_queries";
 import { GiveWellStamp } from "../components/main/blocks/GiveWellStamp/GiveWellStamp";
 import { WealthCalculatorTeaser } from "../components/main/blocks/WealthCalculatorTeaser/WealthCalculatorTeaser";
 import { SplitView } from "../components/main/blocks/SplitView/SplitView";
@@ -201,9 +201,7 @@ const fetchFrontpage = groq`
     introsection,
     malariaday {
       ...,
-      links[] {
-        ${linksSelectorQuery}
-      }
+      ${linksContentQuery}
     },
     intervention_widget,
     key_points,
