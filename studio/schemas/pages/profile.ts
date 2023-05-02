836573d6637079a085a95c532940e6113006ca0a
Adam Alfredsson <alfredsson.adam@gmail.com>
2023-05-02 17:22:41 +0200

    Restore missing tax deduction field

--- studio/schemas/pages/profile.ts
+++ studio/schemas/pages/profile.ts
@@ -4,6 +4,12 @@ export default {
   type: "document",
   fields: [
     {
+      name: "tax",
+      title: "Tax deduction",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
       name: "data",
       title: "Data policy",
       type: "array",
