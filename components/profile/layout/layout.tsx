57f628967db738b3a8308787449302196e11c5b4
Philip <44257880+philiphand@users.noreply.github.com>
2023-05-21 19:54:21 +0200

    Restores old layout and fixes "Logg ut" button

--- components/profile/layout/layout.tsx
+++ components/profile/layout/layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 import Footer from "../../shared/layout/Footer/Footer";
 import styles from "../../shared/layout/Layout/Layout.module.scss";
 import { Auth0Provider, CacheLocation } from "@auth0/auth0-react";
@@ -12,8 +12,8 @@ import { SWRConfig } from "swr";
 import { CookiesAccepted, WidgetContext } from "../../main/layout/layout";
 import { WidgetPane } from "../../main/layout/WidgetPane/WidgetPane";
 import { useRouterContext } from "../../../context/RouterContext";
-import { MissingNameModal } from "./MissingNameModal/MissingNameModal";
 import { PreviewBlock } from "../../main/layout/PreviewBlock/PreviewBlock";
+import { MissingNameModal } from "./MissingNameModal/MissingNameModal";
 
 const createRedirectCallback = (dashboardPath: string[]) => (appState: any) => {
   Router.replace(appState?.returnTo || dashboardPath.join("/"));
@@ -65,32 +65,34 @@ export const ProfileLayout: React.FC<LayoutProps> = ({
     >
       <div className={styles.container + " " + styles.dark}>
         <SWRConfig value={{ revalidateOnFocus: false, shouldRetryOnError: false }}>
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={true}
-            newestOnTop={true}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            closeButton={false}
-            toastStyle={{ borderRadius: 0, background: "white", color: "black" }}
-          />
-          <WidgetContext.Provider value={[widgetOpen, setWidgetOpen]}>
-            <CookiesAccepted.Provider value={[cookiesAccepted, setCookiesAccepted]}>
-              <WidgetPane darkMode={true} text={widgetData} />
-              <main className={styles.main}>
-                <UserWrapper skipAuthentication={bypassAuth}>
-                  <DonorProvider>
-                    <ActivityProvider>{children}</ActivityProvider>
-                  </DonorProvider>
-                </UserWrapper>
-              </main>
-            </CookiesAccepted.Provider>
-          </WidgetContext.Provider>
-          <Footer {...footerData} />
+          <UserWrapper skipAuthentication={bypassAuth}>
+            <DonorProvider>
+              <ActivityProvider>
+                <ToastContainer
+                  position="top-right"
+                  autoClose={3000}
+                  hideProgressBar={true}
+                  newestOnTop={true}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  closeButton={false}
+                  toastStyle={{ borderRadius: 0, background: "white", color: "black" }}
+                />
+                {isPreview && <PreviewBlock />}
+                <WidgetContext.Provider value={[widgetOpen, setWidgetOpen]}>
+                  <CookiesAccepted.Provider value={[cookiesAccepted, setCookiesAccepted]}>
+                    <WidgetPane darkMode={true} text={widgetData} />
+                    <main className={styles.main}>{children}</main>
+                    <MissingNameModal />
+                  </CookiesAccepted.Provider>
+                </WidgetContext.Provider>
+                <Footer {...footerData} />
+              </ActivityProvider>
+            </DonorProvider>
+          </UserWrapper>
         </SWRConfig>
       </div>
     </Auth0Provider>
