3caee524f297ecdb3004ada0c6a839170b0a8e22
Håkon Harnes <account@harnes.me>
2023-06-12 14:12:00 +0200

    Fixes sanity config typing issue

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,17 +1,17 @@
 // lib/sanity.js
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { createPreviewSubscriptionHook, createCurrentUserHook } from "next-sanity";
-import { config } from "./config";
+import { projectConfig } from "./config";
 import imageUrlBuilder from "@sanity/image-url";
 
 /**
  * Set up a helper function for generating Image URLs with only the asset reference data in your documents.
  * Read more: https://www.sanity.io/docs/image-url
  **/
-export const urlFor = (source: SanityImageSource) => imageUrlBuilder(config).image(source);
+export const urlFor = (source: SanityImageSource) => imageUrlBuilder(projectConfig).image(source);
 
 // Set up the live preview subscription hook
-export const usePreviewSubscription = createPreviewSubscriptionHook(config);
+export const usePreviewSubscription = createPreviewSubscriptionHook(projectConfig);
 
 // Helper function for using the current logged in user account
-export const useCurrentUser = createCurrentUserHook(config);
+export const useCurrentUser = createCurrentUserHook(projectConfig);
