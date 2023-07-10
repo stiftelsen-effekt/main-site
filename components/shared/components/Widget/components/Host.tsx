1f8df77e2c185b918470a4072de1beb1e78512d8
Adam Alfredsson <alfredsson.adam@gmail.com>
2023-07-10 16:39:35 +0200

    Remove unused Host component

--- components/shared/components/Widget/components/Host.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { WidgetWrapper } from "./Host.style";
-
-export const Host: React.FC = ({ children }) => {
-  return <WidgetWrapper>{children}</WidgetWrapper>;
-};
