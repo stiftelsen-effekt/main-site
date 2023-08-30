36126dfd0dda23923bb484b928457c553bc94e8d
Adam Alfredsson <alfredsson.adam@gmail.com>
2023-08-30 13:12:04 +0200

    Integrate with 404

--- pages/404.page.tsx
+++ pages/404.page.tsx
@@ -6,17 +6,14 @@ import { SectionContainer } from "../components/main/layout/SectionContainer/sec
 import { Navbar } from "../components/main/layout/navbar";
 import { CookieBanner } from "../components/shared/layout/CookieBanner/CookieBanner";
 import { MainHeader } from "../components/shared/layout/Header/Header";
-import { getClient } from "../lib/sanity.server";
 import { getAppStaticProps } from "./_app.page";
 
-const Custom404: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ data, preview }) => {
-  const settings = data.result.settings[0];
-
+const Custom404: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ navbar }) => {
   return (
     <>
       <MainHeader hideOnScroll={true}>
         <CookieBanner />
-        <Navbar logo={settings.logo} elements={settings["main_navigation"]} />
+        <Navbar {...navbar} />
       </MainHeader>
 
       <SectionContainer>
@@ -28,44 +25,13 @@ const Custom404: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ d
 
 export const getStaticProps = async ({ preview = false }: GetStaticPropsContext) => {
   const appStaticProps = await getAppStaticProps({ preview });
-  let result = await getClient(preview).fetch(fetchNotFoundPage);
 
   return {
     props: {
       appStaticProps,
-      preview: preview,
-      data: {
-        result,
-        query: fetchNotFoundPage,
-        queryParams: {},
-      },
+      navbar: await Navbar.getStaticProps({ preview }),
     },
   };
 };
-
-const fetchNotFoundPage = groq`
-{
-  "settings": *[_type == "site_settings"] {
-    logo,
-    main_navigation[] {
-      _type == 'navgroup' => {
-        _type,
-        _key,
-        title,
-        items[]->{
-          title,
-          "slug": page->slug.current
-        },
-      },
-      _type != 'navgroup' => @ {
-        _type,
-        _key,
-        title,
-        "slug": page->slug.current
-      },
-    }
-  },
-}
-`;
 
 export default Custom404;
