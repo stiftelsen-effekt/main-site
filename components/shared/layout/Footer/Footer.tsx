9bc56344fd7e809060401529da57bbb3be1e5b23
Håkon Harnes <account@harnes.me>
2023-11-22 15:29:20 +0100

    Makes footer column count dynamic

--- components/shared/layout/Footer/Footer.tsx
+++ components/shared/layout/Footer/Footer.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { LinkType } from "../../../main/blocks/Links/Links";
 import { NavLink } from "../../../main/layout/navbar";
-import vercelBanner from "../../../../public/vercel-banner.svg";
 import styles from "./Footer.module.scss";
 import { NewsletterSignup } from "../../../main/blocks/NewsletterSignup/NewsletterSignup";
 import { withStaticProps } from "../../../../util/withStaticProps";
@@ -12,167 +10,94 @@ import { groq } from "next-sanity";
 
 export type FooterItem = NavLink | LinkType;
 export type FooterProps = {
-  footer_column_1?: FooterItem[];
-  footer_column_2?: FooterItem[];
-  footer_column_3?: FooterItem[];
-  footer_column_4?: FooterItem[];
-};
+  _key: string;
+  links?: FooterItem[];
+}[];
 
 type QueryResult = {
-  footer: [FooterProps];
+  data: {
+    _id: string;
+    footer_columns?: FooterProps;
+  }[];
 };
 
-const query = groq`
+export const footerQuery = groq`
   {
-    "footer": *[_type == "site_settings"] {
-      footer_column_1[] {
-        _type == 'navitem' => @ {
-          ...,
-          "slug": page->slug.current
-        },
-        _type == 'link' => @ {
-          ...
-        },
-      },
-      footer_column_2[] {
-        _type == 'navitem' => @ {
-          ...,
-          "slug": page->slug.current
-        },
-        _type == 'link' => @ {
-          ...
-        },
-      },
-      footer_column_3[] {
-        _type == 'navitem' => @ {
-          ...,
-          "slug": page->slug.current
-        },
-        _type == 'link' => @ {
-          ...
-        },
-      },
-      footer_column_4[] {
-        _type == 'navitem' => @ {
-          ...,
-          "slug": page->slug.current
-        },
-        _type == 'link' => @ {
-          ...
-        },
+    "data": *[_type == "site_settings"] {
+      _id,
+      footer_columns[] {
+        _key,
+        links[] {
+          _type == 'navitem' => @ {
+            ...,
+            "slug": page->slug.current
+          },
+          _type == 'link' => @ {
+            ...
+          },
+        }
       },
     }
   }
 `;
 
 const Footer = withStaticProps(async ({ preview }: { preview: boolean }) => {
-  const result = await getClient(preview).fetch<QueryResult>(query);
+  const result = await getClient(preview).fetch<QueryResult>(footerQuery);
 
+  const footer = result.data[0];
   return {
-    data: result.footer[0],
+    data: footer,
   };
-})(({ data: { footer_column_1, footer_column_2, footer_column_3, footer_column_4 } }) => {
+})(({ data: { footer_columns } }) => {
+  const columnCount = footer_columns ? footer_columns.filter((c) => c.links).length : 0;
+
+  const gridClass = styles[`grid_${columnCount}`];
+
   return (
-    <footer className={styles.grid} id={"footer"}>
+    <footer className={`${styles.grid} ${gridClass}`} id={"footer"}>
       <div className={`${styles.category} ${styles.logo__bottom}`}>
         <figure>
           <p>G</p>
         </figure>
       </div>
-
-      <div className={`${styles.category} ${styles.primary__links}`}>
-        <ul>
-          {footer_column_1 &&
-            footer_column_1.map((footerItem) => {
-              return (
-                <li key={footerItem._key}>
-                  <Link
-                    href={
-                      footerItem._type === "navitem" ? `/${footerItem.slug}` : footerItem.url || "/"
-                    }
-                    passHref
-                  >
-                    <a target={footerItem._type === "link" && footerItem.newtab ? "_blank" : ""}>
-                      {footerItem.title}
+      {footer_columns &&
+        footer_columns.map((column, index) => (
+          <div className={`${styles.category}`} key={column._key}>
+            <ul>
+              {column.links &&
+                column.links.map((footerItem) => {
+                  return (
+                    <li key={footerItem._key}>
+                      <Link
+                        href={
+                          footerItem._type === "navitem"
+                            ? `/${footerItem.slug}`
+                            : footerItem.url || "/"
+                        }
+                        passHref
+                      >
+                        <a
+                          target={footerItem._type === "link" && footerItem.newtab ? "_blank" : ""}
+                        >
+                          {footerItem.title}
+                        </a>
+                      </Link>
+                    </li>
+                  );
+                })}
+              {index === columnCount - 1 && (
+                <>
+                  <li>&nbsp;</li>
+                  <li>
+                    <a data-cy="navigate-to-top" href="#top">
+                      Til toppen &uarr;
                     </a>
-                  </Link>
-                </li>
-              );
-            })}
-        </ul>
-      </div>
-
-      <div className={`${styles.category} ${styles.secondary__links}`}>
-        <ul>
-          {footer_column_2 &&
-            footer_column_2.map((footerItem) => {
-              return (
-                <li key={footerItem._key}>
-                  <Link
-                    href={
-                      footerItem._type === "navitem" ? `/${footerItem.slug}` : footerItem.url || "/"
-                    }
-                    passHref
-                  >
-                    <a target={footerItem._type === "link" && footerItem.newtab ? "_blank" : ""}>
-                      {footerItem.title}
-                    </a>
-                  </Link>
-                </li>
-              );
-            })}
-        </ul>
-      </div>
-
-      <div className={`${styles.category} ${styles.tertiary__links}`}>
-        <ul>
-          {footer_column_3 &&
-            footer_column_3.map((footerItem) => {
-              return (
-                <li key={footerItem._key}>
-                  <Link
-                    href={
-                      footerItem._type === "navitem" ? `/${footerItem.slug}` : footerItem.url || "/"
-                    }
-                    passHref
-                  >
-                    <a target={footerItem._type === "link" && footerItem.newtab ? "_blank" : ""}>
-                      {footerItem.title}
-                    </a>
-                  </Link>
-                </li>
-              );
-            })}
-        </ul>
-      </div>
-
-      <div className={`${styles.category} ${styles.utillity}`}>
-        <ul>
-          {footer_column_4 &&
-            footer_column_4.map((footerItem) => {
-              return (
-                <li key={footerItem._key}>
-                  <Link
-                    href={
-                      footerItem._type === "navitem" ? `/${footerItem.slug}` : footerItem.url || "/"
-                    }
-                    passHref
-                  >
-                    <a target={footerItem._type === "link" && footerItem.newtab ? "_blank" : ""}>
-                      {footerItem.title}
-                    </a>
-                  </Link>
-                </li>
-              );
-            })}
-          <li>&nbsp;</li>
-          <li>
-            <a data-cy="navigate-to-top" href="#top">
-              Til toppen &uarr;
-            </a>
-          </li>
-        </ul>
-      </div>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        ))}
       <div className={styles.newsletter}>
         <NewsletterSignup></NewsletterSignup>
       </div>
@@ -180,12 +105,6 @@ const Footer = withStaticProps(async ({ preview }: { preview: boolean }) => {
         Structured content powered by{" "}
         <a href="https://www.sanity.io/" target="_blank" rel="noreferrer">
           Sanity.io
-        </a>
-        <a
-          href="https://vercel.com?utm_source=effective-altruism-norway&utm_campaign=oss"
-          style={{ borderBottom: "none", marginTop: "0.8rem", display: "block" }}
-        >
-          <Image src={vercelBanner} alt="Powered by Vercel" width={160} />
         </a>
       </div>
     </footer>
