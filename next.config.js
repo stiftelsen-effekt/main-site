740081d4e3411e447e93c15b71ec0a8ae726e76e
Håkon Harnes <account@harnes.me>
2024-03-21 14:14:24 +0100

    Updates csp

--- next.config.js
+++ next.config.js
@@ -23,11 +23,11 @@ const nextConfig = {
   headers: async () => {
     return [
       {
-        source: "(/.*)",
+        source: "/:path*",
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "frame-ancestors 'none';",
+            value: "frame-ancestors 'none'",
           },
         ],
       },
