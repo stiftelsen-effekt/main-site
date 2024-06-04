9dfb6380b19211b095021cd47991ae4a2253fb19
Håkon Harnes <account@harnes.me>
2024-06-04 20:08:51 +0200

    Remove forced client stega

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -4,10 +4,10 @@ import { projectConfig } from "./config";
 export function getClient(previewToken?: string): SanityClient {
   return createClient({
     ...projectConfig,
-    useCdn: false, // !previewToken,
+    useCdn: !previewToken,
     perspective: "previewDrafts",
     stega: {
-      enabled: true, //previewToken ? true : false,
+      enabled: previewToken ? true : false,
       studioUrl: "http://localhost:3333",
     },
     token: previewToken,
