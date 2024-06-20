65b08fa8b55d3559cc2fff4ddebd56e821ada622
Håkon Harnes <account@harnes.me>
2024-06-20 17:02:07 +0200

    Removes hard coded read token

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,6 +1,10 @@
 import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
+  api: {
+    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_API_DATASET,
+  },
   project: {
     basePath: "/studio",
   },
