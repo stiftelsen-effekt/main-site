5fd1afcc3c30019cd859ff616fa3974436d907ed
Håkon Harnes <account@harnes.me>
2024-09-02 11:15:45 +0200

    Fixes responsive image component

--- components/shared/responsiveimage.tsx
+++ components/shared/responsiveimage.tsx
@@ -5,19 +5,19 @@ import Image from "next/image";
 import { getClient } from "../../lib/sanity.client";
 
 export const ResponsiveImage: React.FC<{
-  image?: SanityImageObject;
+  image: SanityImageObject;
   alt?: string;
   onClick?: () => void;
   priority?: boolean;
   sizes?: string;
   layout?: "intrinsic" | "fill" | "responsive" | "fixed" | "cover";
 }> = ({ image, alt, onClick, priority, sizes, layout = "fill" }) => {
+  const sanityClient = getClient();
+  let imageProps = useNextSanityImage(sanityClient, image);
+
   if (!image) {
     return null;
   }
-
-  const sanityClient = getClient();
-  let imageProps = useNextSanityImage(sanityClient, image);
 
   let lqip = null;
   if (
