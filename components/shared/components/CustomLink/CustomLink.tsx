5276c4e56a9f36de669912880bf1e660b79dd091
Håkon Harnes <account@harnes.me>
2024-12-09 19:41:56 +0100

    Disable prefetch

--- components/shared/components/CustomLink/CustomLink.tsx
+++ components/shared/components/CustomLink/CustomLink.tsx
@@ -7,7 +7,7 @@ interface CustomLinkProps extends React.ComponentProps<typeof Link> {
 
 export const CustomLink = ({ href, ...props }: CustomLinkProps) => {
   if (href === "/") {
-    return <Link href="/[state]/index" as="/" {...props} />;
+    return <Link href="/[state]/index" as="/" {...props} prefetch={false} />;
   }
 
   let cleanHref;
