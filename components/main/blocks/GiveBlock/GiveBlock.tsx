bdee625911e6a0628360c95012365edb355a72fd
Cole Bittel <cole.bittel@pm.me>
2025-08-15 13:04:44 +0200

    back to gi
    Signed-off-by: Cole Bittel <cole.bittel@pm.me>

--- components/main/blocks/GiveBlock/GiveBlock.tsx
+++ components/main/blocks/GiveBlock/GiveBlock.tsx
@@ -51,7 +51,7 @@ export const GiveBlock: React.FC<GiveBlockProps> = ({
         }}
         style={accentStyles}
       >
-        {donateLabel || "Giv."}
+        {donateLabel || "Gi."}
       </button>
     </div>
   );
