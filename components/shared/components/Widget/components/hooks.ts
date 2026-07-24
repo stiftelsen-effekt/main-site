87084f3e7e36fc5aba3d3406636aa5d2aaf708eb
Max Baz <max@baz.nu>
2026-07-25 01:09:40 +0200

    dont set distribution custom when only 1 option

--- components/shared/components/Widget/components/hooks.ts
+++ components/shared/components/Widget/components/hooks.ts
@@ -203,7 +203,12 @@ const handlePrefilledCauseArea = (
   prefilledCauseArea: PrefilledDistribution[number],
   causeAreaAmount: number,
 ): Record<number, number> => {
-  dispatch(setCauseAreaDistributionType(causeArea.id, ShareType.CUSTOM));
+  dispatch(
+    setCauseAreaDistributionType(
+      causeArea.id,
+      causeArea.organizations.length <= 1 ? ShareType.STANDARD : ShareType.CUSTOM,
+    ),
+  );
 
   const shareByOrgId: Record<number, number> = {};
   prefilledCauseArea.organizations.forEach((org) => {
